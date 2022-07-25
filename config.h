723fdce04bafbbe1c5bcd03c9e0905d4b5ce6cc4
Gabriel <gabrielramondev23@gmail.com>
2022-07-24 19:21:14 -0500

    ajustes en el muestreo y envio de datos

---
+++ config.h
@@ -0,0 +1,6 @@
+// const char *ssid = "Ubee72B3-2.4G";
+// const char *password = "NFOYQQU30E";
+const char *ssid = "Software";
+const char *password = "software22";
+
+const char *url_server = "https://192.168.0.9/api/index.php";
--- smartGHFinal.ino
+++ smartGHFinal.ino
@@ -3,10 +3,14 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#define bomba 23
-#define sensorHum 13
-#define echoPin 12
-#define trigPin 14
+#include <esp_wifi.h>
+#include "config.h"
+#define dht11 23
+#define sensorHum 32
+#define echoPin 15
+#define trigPin 4
+#define bomba 22
+
 byte aguaVacia[8] = {
     0b01010,
     0b11011,
@@ -98,26 +102,26 @@ byte aguaBaja[] = {
     B11111,
     B11111};
 
-DHT dht(4, DHT11);
+DHT dht(dht11, DHT11);
 
-const int rs = 15, en = 16, d4 = 17, d5 = 5, d6 = 18, d7 = 19; // vo = potenciometro, vdd = 5V, k = ground, a = 5V,rw = ground, vss = ground
+const int rs=13 , en=12 , d4=14 , d5=27, d6=26, d7=25; // vo = potenciometro, vdd = 5V, k = ground, a = 5V,rw = ground, vss = ground
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-const char *ssid = "Ubee72B3-2.4G";
-const char *password = "NFOYQQU30E";
-
+//ip estática
 IPAddress ip_local(192,168,0,26);
-IPAddress gateway(192,168,0,1);
+IPAddress gateway(192,168,0,1); 
 IPAddress subnet(255,255,255,0);
+IPAddress primaryDNS(8,8,8,8);
+IPAddress secondaryDNS(8,8,4,4);
 
-const char *url = "http://192.168.89.216/prueba/esp32-post.php";
+//mac personalizada
+// uint8_t newMACAddress[] = {0x30, 0x45, 0x96, 0x1F, 0xBE, 0x2E};
 
 float t, h, humS, hS, agua;
 long seg, distance;
 int bombaEstado = 0;
 
-unsigned long time1, time2;
-int intervaloLecturas = 0, intervaloActualizacion = 0;
+unsigned long time1;
+int intervaloLecturas = 0;
 
 void setup()
 {
@@ -130,20 +134,23 @@ void setup()
   lcd.createChar(3, gota);
   lcd.createChar(4, humSuelo);
   lcd.clear();
-  pinMode(bomba, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   digitalWrite(trigPin, LOW);
+  pinMode(bomba, OUTPUT);
   digitalWrite(bomba, LOW);
   bombaEstado = 0;
-
-  if (!WiFi.config(ip_local, gateway, subnet)) {
+  WiFi.mode(WIFI_STA);
+  
+  if (!WiFi.config(ip_local, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("Error en configuracion");
   }
   
   WiFi.begin(ssid, password);
 
   Serial.print("Conectando...");
+  lcd.setCursor(0,0);
+  lcd.print("Conectando......");
   while (WiFi.status() != WL_CONNECTED) { //Check for the connection
     delay(500);
     Serial.print(".");
@@ -151,33 +158,39 @@ void setup()
 
   Serial.print("Conectado con éxito, mi IP es: ");
   Serial.println(WiFi.localIP());
+  Serial.print("[OLD] ESP32 MAC Address:  ");
   Serial.println(WiFi.macAddress());
+  lcd.print("Conectado :)");
+  delay(500);
+  lcd.clear();
+  //configuramos la mac personalizada
+  // esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
+  // Serial.print("[NEW] ESP32 MAC Address:  ");
+  // Serial.println(WiFi.macAddress());
+  
 }
 
 void loop()
 {
-  time1 = millis(); // cada 15 segundos actualizar datos
-  time2 = millis();
+  time1 = millis(); // cada 5 segundos actualizar datos
+  
+  t = dht.readTemperature();
+  h = dht.readHumidity();
 
-  if (time2 - intervaloActualizacion >= 2000)
-  {
-    intervaloActualizacion = time2;
-    t = dht.readTemperature();
-    h = dht.readHumidity();
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    seg = pulseIn(echoPin, HIGH);
-    distance = seg / 59; // 2 cm = 100%, 25cm = 0%
-    agua = ((25.0 - distance) * 100.0) / 23.0;
-    humS = analogRead(sensorHum); // 4095 seco y 1554 humedad perfecta
-    hS = ((4095.0 - humS) * 100.0) / 2541.0;
-    Serial.println("Temperatura: " + String(t) + "°C " + "Humedad: " + String(h) + "%");
-    Serial.println("Humedad de suelo: " + String(hS) + "% Nivel de agua: " + String(agua) + "%");
-    Serial.println("Distancia:" + String(distance) + "cm Estado bomba: " + String(bombaEstado) + "\n");
-  }
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  seg = pulseIn(echoPin, HIGH);
+  distance = seg / 59; // 2 cm = 100%, 25cm = 0%
+  agua = ((25.0 - distance) * 100.0) / 23.0;
 
-  if (hS > 100.0)
+  humS = analogRead(sensorHum); // 4095 seco y 1554 humedad perfecta
+  hS = ((4095.0 - humS) * 100.0) / 2541.0;
+
+  Serial.println("Temperatura: " + String(t) + "°C " + "Humedad: " + String(h) + "%");
+  Serial.println("Humedad de suelo: " + String(hS) + " Nivel de agua: " + String(agua) + "%");
+  Serial.println("Distancia:" + String(distance) + "cm Estado bomba: " + String(bombaEstado) + "\n");
+   if (hS > 100.0)
   {
     hS = 100.0;
   }
@@ -194,41 +207,44 @@ void loop()
   {
     agua = 0.0;
   }
-
+  mostrarHumedadSuelo();
+  mostrarNivelAgua();
+  mostrarDHT11();
   if (bombaEstado == 0)
-  { // si la bomba esta apagada
-    mostrarDHT11();
-    mostrarHumedadSuelo();
-    mostrarNivelAgua();
+  {
     if (hS <= 49 && agua >= 15)
     {
       bombaEstado = 1;
-      lcd.clear();
     }
   }
   else
   {
-    regar();
+    regar(agua);
   }
-
-  if (time1 - intervaloLecturas >= 15000)
+   if (time1 - intervaloLecturas >= 5000)
   {
     intervaloLecturas = time1;
-    mandarDatos();
+    String datos = "METHOD=DATOS&temp=" + String(t) + "&hum=" + String(h) + "&humS=" + String(hS) + "&nivelAgua=" + String(agua) + "&bomba=" + String(bombaEstado);
+    mandarDatos(datos);
   }
+  delay(1000);
 }
-void regar()
+void regar(float agua)
 {
+  String datos = "METHOD=RIEGO&nivelAgua=" + String(agua); 
   digitalWrite(bomba, HIGH);
   lcd.setCursor(1,0);
   lcd.print("R E G A N D O");
   mostrarHumedadSuelo();
   mostrarNivelAgua();
+  Serial.println("\nREGADO\n");
   if (hS >= 50 || agua <= 0)
   {
     digitalWrite(bomba, LOW);
     bombaEstado = 0;
+    lcd.clear();
   }
+  mandarDatos(datos);
 }
 
 void mostrarDHT11()
@@ -250,8 +266,6 @@ void mostrarHumedadSuelo()
   // mostrar humedad de suelo
   if (hS <= 0.0)
   {
-
-
       lcd.setCursor(0, 1);
       lcd.write(byte(4));
       lcd.print(hS);
@@ -287,7 +301,6 @@ void mostrarNivelAgua()
   // mostrar nivel de agua
   if (agua <= 0.0)
   {
-    
       lcd.createChar(5, aguaVacia);
       lcd.setCursor(9, 1);
       lcd.write(byte(5));
@@ -336,15 +349,13 @@ void mostrarNivelAgua()
   }
 }
 
-void mandarDatos(){
+void mandarDatos(String datos){
   if (WiFi.status() == WL_CONNECTED)
   { // Check WiFi connection status
     HTTPClient http;
-    String datos = "temp=" + String(t) + "&hum=" + String(h) + "&humS=" + String(hS) + "&nivelAgua=" + String(agua) + "&bomba=" + String(bombaEstado);
-    Serial.println(datos);
-    http.begin(url);
+    http.begin(url_server);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // Preparamos el header text/plain si solo vamos a enviar texto plano sin un paradigma llave:valor.
-
+    Serial.println(datos);
     int response = http.POST(datos); // Enviamos el post pasándole, los datos que queremos enviar. (esta función nos devuelve un código que guardamos en un int)
 
     if (response > 0)
